29b01c65f6ca87df38a25e60e11f2b4b0155b8ed
thiyangt <thiyanga.talagala@monash.edu>
2019-10-30 06:22:58 +1030

    done!

--- src/main.R
+++ src/main.R
@@ -3,3 +3,18 @@
 ##devtools::install_github("libjohn/dukeslides")
 library(xaringan)
 library(dukeslides)
+
+
+## saveGIF
+library(animation)
+library(tourr)
+library(seer)
+library(Mcomp)
+data(M1)
+yearly_m1 <- subset(M1, "yearly")
+M1yearly_features <- seer::cal_features(yearly_m1, database="M1", h=6, highfreq = FALSE)
+saveGIF(animate(M1yearly_features[, 1:25], grand_tour(d = 2), display = display_xy()))
+if(Sys.getenv("RSTUDIO") == "1" & # check if running in RStudio
+   .Platform$OS.type == "unix") quartz() else X11()
+quartz.save(animate(M1yearly_features[, 1:25], grand_tour(d = 2), display = display_xy()), type = "gif")
+
